6685540985dc70894844b6797b350b4e26d9e3bf
Yan Carlos Escobar Pupo <escobarpupoyancarlos1@gmail.com>
2024-04-18 18:41:38 -0400

    feat: add first component for 100 Days CSS Challenge

---
+++ src/app/components/day1/page.tsx
@@ -0,0 +1,18 @@
+import "./ChalangeComponent.css";
+
+export default function ChalangeComponent() {
+    return (
+        <div className="cartel">
+            <div className="text">
+                <div className="numbers">
+                <div className="one-one"></div>
+                <div className="one-two"></div>
+                <div className="zero-one"></div>
+                <div className="zero-two"></div>
+                </div>
+                <span className="text2">Days</span>
+                <span className="text3">CSS Challenge</span>
+            </div>
+        </div>
+    )
+}
